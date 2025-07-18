ebc6ffcdc0e173b9d883ff33eb7fff7337ee2b5c
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-07-18 17:23:15 +0530

    auth pages added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,39 @@
-import { AuthProvider } from "@/lib/auth-context";
-import { Stack, useRouter } from "expo-router";
-// import { useEffect } from "react"; 
+import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
+import { useEffect } from "react";
 
-// function RouteGuard({children}:{children: React.ReactNode}){
-//   const router = useRouter();
-//   const isAuth = false;
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { user, isLoadingUser } = useAuth();
+  const segments = useSegments();
 
-//   useEffect(()=>{
-//     if(!isAuth){
-//       router.replace("/auth")
-//     }
-//   },[])
-//   return <> {children}</>
-// }
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "auth";
+
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/");
+    }
+  }, [user, segments]);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
-      </Stack>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <RouteGuard>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+          </RouteGuard>
+        </SafeAreaProvider>
+      </PaperProvider>
     </AuthProvider>
-  )
+  );
 }
