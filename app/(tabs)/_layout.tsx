2ec64e68c5d1fa9b4b0c66fed4754fa60a185f02
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-07-18 17:13:24 +0530

    auth added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,17 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const TabsLayout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name='index' options={{title:"Home"}}/>
+    <Tabs screenOptions={{tabBarActiveTintColor:'coral'}}>
+        <Tabs.Screen name='index' options={{
+            title:"Home",
+            tabBarIcon:({color})=>(
+                <FontAwesome name="home" size={24} color={color} />
+            ),
+        }}/>
         <Tabs.Screen name='login' options={{title:"Login"}}/>
     </Tabs>
   )
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,25 @@
-import { Stack } from "expo-router";
+import { AuthProvider } from "@/lib/auth-context";
+import { Stack, useRouter } from "expo-router";
+// import { useEffect } from "react"; 
+
+// function RouteGuard({children}:{children: React.ReactNode}){
+//   const router = useRouter();
+//   const isAuth = false;
+
+//   useEffect(()=>{
+//     if(!isAuth){
+//       router.replace("/auth")
+//     }
+//   },[])
+//   return <> {children}</>
+// }
 
 export default function RootLayout() {
   return (
-  <Stack>
-    <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
-  </Stack> 
-)
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
+      </Stack>
+    </AuthProvider>
+  )
 }
