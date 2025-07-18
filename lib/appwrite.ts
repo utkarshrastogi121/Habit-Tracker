76bf27c6c6330cf2e7c4ed7d3d86f06cd3410715
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-07-18 15:40:57 +0530

    Appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+import {Account, Client, Databases} from 'react-native-appwrite'
+
+export const client = new Client()
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.EXPO_PUBLIC_PROJECT_ID)
+    .setPlatform(process.env.EXPO_PUBLIC_PLATFORM);
+
+export const account = new Account(client)
